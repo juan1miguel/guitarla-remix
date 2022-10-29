aac47a16ef1e8255d4c9f0118024d5d7c9b5d887
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-29 18:16:05 -0400

    agregando logo al header

--- app/components/header.jsx
+++ app/components/header.jsx
@@ -1,10 +1,13 @@
 import { Link } from "@remix-run/react";
-
+import Logo from '../../public/img/logo.svg';
 function Header() {
   return (
     <header className="header">
       <div className="contenedor barra">
-        <div className="logo"></div>
+        <Link to='/'>
+
+            <img  className="logo" src={Logo} alt="imagen de logo" />
+        </Link>
         <nav className="navegacion">
           <Link to="/">Inicio</Link>
           <Link to="/nosotros">Nosotros</Link>
