54bff632621a4050e214c997d846fcc4885704d0
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-31 13:25:41 -0400

    consumiendo al backend de strapi con loader

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,10 +1,13 @@
-
+export async function loader() {
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  );
+  const resultado = await respuesta.json();
+  
+  return {};
+}
 function Tienda() {
-  return (
-    <div>
-      
-    </div>
-  )
+  return <div></div>;
 }
 
-export default Tienda
+export default Tienda;
