1ebe310e0374f953f39e97bfc57dd8107650a83b
Josue Alvarez <josuealvarez348@gmail.com>
2022-10-31 14:12:04 -0400

    mostrando informacion de la guitarra

--- app/components/guitarra.jsx
+++ app/components/guitarra.jsx
@@ -1,9 +1,23 @@
+import { Link } from "@remix-run/react";
+function Guitarra({ guitarra }) {
+  const { descripcion, imagen, precio, url, nombre } = guitarra;
 
-function Guitarra({guitarra}) {
-    console.log(guitarra)
   return (
-    <div>Guitarras</div>
-  )
+    <div className="guitarra">
+      <img
+        src={imagen.data.attributes.formats.medium.url}
+        alt={`imagen de guitarra ${nombre} `}
+      />
+      <div className="contenido">
+        <h3>{nombre} </h3>
+        <p className="descripcion">{descripcion} </p>
+        <p className="precio">${precio} </p>
+        <Link className="enlace" to={`/guitarras/${url}`}>
+          Ver Producto
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Guitarra
+export default Guitarra;
