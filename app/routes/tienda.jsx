b8ec282e34c563604664908c4e5c276d5a376dd4
Josue Alvarez <josuealvarez348@gmail.com>
2022-11-01 18:41:23 -0400

    agregando css al listado de guitrras

--- app/routes/tienda.jsx
+++ app/routes/tienda.jsx
@@ -1,23 +1,41 @@
-import {useLoaderData} from '@remix-run/react'
+import { useLoaderData } from "@remix-run/react";
 import { getGuitarras } from "../models/guitarras.server";
-import Guitarra from '~/components/guitarra';
+import Guitarra from "~/components/guitarra";
+import styles from "../styles/guitarras.css";
+export function meta(){
+  return{
+    title: 'GuitarLA - Tienda de Guitarras',
+    description:'GuitarLA - Nuestra coleccionde Guitrras'
+  }
+}
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
 export async function loader() {
   const guitarras = await getGuitarras();
 
   return guitarras.data;
 }
 function Tienda() {
-  const guitarras = useLoaderData()
+  const guitarras = useLoaderData();
 
-  return (<main className='contenedor'>
-    <h2 className='heading'>Nuestra Coleccion</h2>
-    {guitarras.length && (<div className='guitarras-grid'>
-      {guitarras.map(guitarra=>( <Guitarra
-        key={guitarra?.id}
-        guitarra={guitarra?.attributes}
-      />))}
-    </div> )}
-  </main>)
+  return (
+    <main className="contenedor">
+      <h2 className="heading">Nuestra Coleccion</h2>
+      {guitarras.length && (
+        <div className="guitarras-grid">
+          {guitarras.map((guitarra) => (
+            <Guitarra key={guitarra?.id} guitarra={guitarra?.attributes} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
 
 export default Tienda;
