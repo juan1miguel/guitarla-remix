bd39f5be651065e67ca5e9bb58d7b41dcb171a7a
Josue Alvarez <josuealvarez348@gmail.com>
2022-11-02 13:59:20 -0400

    mostrando informacion de la guitarra

--- app/routes/guitarras/$guitarraUrl.jsx
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -1,15 +1,37 @@
-export async function loader({request,params}){
-    console.log('reques', request)
-    console.log('params', params)
-    return {}
+import { useLoaderData } from "@remix-run/react";
+import { getGuitarra } from "../../models/guitarras.server";
+import styles from '~/styles/guitarras.css';
 
+export function links(){
+    return[
+        {
+            rel:'stylesheet',
+            href:styles
+        }
+    ]
+}
+export async function loader({ request, params }) {
+  const { guitarraUrl } = params;
+  const guitarra = await getGuitarra(guitarraUrl);
+  return guitarra;
 }
 function Guitarra() {
+  const guitarra = useLoaderData();
+  const { nombre, descripcion, imagen, precio } = guitarra.data[0].attributes;
   return (
-    <div>
-      $guitarraUrl
-    </div>
-  )
+    <main className="contenedor guitarra">
+      <img
+        className="imagen"
+        src={imagen.data.attributes.url}
+        alt={`imagen de la guitara ${nombre}`}
+      />
+      <div className="contenido">
+        <h3>{nombre} </h3>
+        <p className="texto">{descripcion}</p>
+        <p className="precio">${precio} </p>
+      </div>
+    </main>
+  );
 }
 
-export default Guitarra
+export default Guitarra;
