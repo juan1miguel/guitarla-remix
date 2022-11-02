f4636bc35b46a29f5a99444bc7bad576930802eb
Josue Alvarez <josuealvarez348@gmail.com>
2022-11-02 11:01:32 -0400

    routing dinamico

---
+++ app/routes/guitarras/$guitarraUrl.jsx
@@ -0,0 +1,15 @@
+export async function loader({request,params}){
+    console.log('reques', request)
+    console.log('params', params)
+    return {}
+
+}
+function Guitarra() {
+  return (
+    <div>
+      $guitarraUrl
+    </div>
+  )
+}
+
+export default Guitarra
