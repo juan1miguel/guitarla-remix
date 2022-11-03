70a7e1414a8f8d1f8d7d1c426e9ccd80e91f5f5f
Josue Alvarez <josuealvarez348@gmail.com>
2022-11-03 13:07:47 -0400

    aplicando css al blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,19 +1,30 @@
 import { getPosts } from "~/models/posts.server";
-import {useLoaderData} from '@remix-run/react';
+import { useLoaderData } from "@remix-run/react";
 import Post from "~/components/post";
+import style from "~/styles/blog.css";
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: style,
+    },
+  ];
+}
 export async function loader() {
   const posts = await getPosts();
   console.log(posts);
-  return posts.data
+  return posts.data;
 }
 function Blog() {
-  const posts = useLoaderData()
- 
+  const posts = useLoaderData();
+
   return (
     <main className="contenedor">
       <h2 className="heading">Blog</h2>
       <div className="blog">
-        {posts.map(post=>(<Post key={post.id} post={post.attributes} />))}
+        {posts.map((post) => (
+          <Post key={post.id} post={post.attributes} />
+        ))}
       </div>
     </main>
   );
