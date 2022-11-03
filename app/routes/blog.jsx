657832c4882e87bbc75bdb724eefd8226bbc3fff
Josue Alvarez <josuealvarez348@gmail.com>
2022-11-03 12:20:24 -0400

    primeros pasos con el blog

--- app/routes/blog.jsx
+++ app/routes/blog.jsx
@@ -1,8 +1,22 @@
-
+import { getPosts } from "~/models/posts.server";
+import {useLoaderData} from '@remix-run/react';
+import Post from "~/components/post";
+export async function loader() {
+  const posts = await getPosts();
+  console.log(posts);
+  return posts.data
+}
 function Blog() {
+  const posts = useLoaderData()
+ 
   return (
-    <div>blog</div>
-  )
+    <main className="contenedor">
+      <h2 className="heading">Blog</h2>
+      <div className="blog">
+        {posts.map(post=>(<Post key={post.id} post={post.attributes} />))}
+      </div>
+    </main>
+  );
 }
 
-export default Blog
+export default Blog;
