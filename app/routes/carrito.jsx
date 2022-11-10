8c021818c92b8edddb6b6fa68b510899bac15b5c
Juan M <juanm0677@gmail.com>
2022-11-10 11:29:07 -0400

    añadiendo el contenido y css

--- app/routes/carrito.jsx
+++ app/routes/carrito.jsx
@@ -1,8 +1,34 @@
-
+import styles from "~/styles/carrito.css";
+export function links() {
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+  ];
+}
+export function meta(){
+    return{
+        title:'GuitarLA - Carrito de Compras',
+        description:'Venta de guitarras, musica,blog, tienda'
+    }
+}
 function Carrito() {
   return (
-    <div>carrito</div>
-  )
+    <main className="contenedor">
+      <h1 className="heading">Carrito de Compras</h1>
+      <div className="contenido">
+        <div className="carrito">
+            <h2>Articulos</h2>
+        </div>
+      <aside className="resumen">
+        <h3>Resumen de Pedido</h3>
+        <p>Total a Pagar</p>
+      </aside>
+
+      </div>
+    </main>
+  );
 }
 
-export default Carrito
+export default Carrito;
